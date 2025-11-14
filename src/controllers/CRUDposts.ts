e86535acef80ea21769b4b132d41f285182da134
tomasbennett <tjs.bennett@gmail.com>
2025-11-14 14:07:24 +0000

    Adding the controller functionality for getting posts without a filter or by post id

--- src/controllers/CRUDposts.ts
+++ src/controllers/CRUDposts.ts
@@ -1,9 +1,9 @@
-import { Request, Response, NextFunction } from "express";
+// import { Request, Response, NextFunction } from "express";
 
-export function ensureAuthentication(req: Request, res: Response, next: NextFunction) {
-    if (req.isAuthenticated && req.isAuthenticated()) {
-        return next();
-    }
+// export function ensureAuthentication(req: Request, res: Response, next: NextFunction) {
+//     if (req.isAuthenticated && req.isAuthenticated()) {
+//         return next();
+//     }
 
-    return res.redirect("/login");
-}
+//     return res.redirect("/login");
+// }
--- src/controllers/posts.ts
+++ src/controllers/posts.ts
@@ -1,49 +1,80 @@
 import { NextFunction, Request, Response, Router } from "express";
-import { ensureAuthentication } from "./CRUDposts";
+import { ensureAuthentication } from "../services/ensureAuthentication";
 import { Prisma } from "@prisma/client";
 import { Posts } from "@prisma/client";
 
 import { prisma } from "../db/prisma";
+import { HTTPError } from "../errors/httpErrors";
 
 
 const router = Router();
 
 
-router.get("/posts", ensureAuthentication, async (req: Request, res: Response, next: NextFunction) => {
+router.get("/", ensureAuthentication, async (req: Request, res: Response, next: NextFunction) => {
     try {
-
         const includeUser: boolean = req.user?.member ?? false;
 
         const allPosts = await prisma.posts.findMany({
             include: { user: includeUser }
         });
 
-
-        return res.status(200).json({
-            status: 200,
-            posts: allPosts.map((post, indx) => {
-                return {
-                    ...post,
-                    user: post?.user ?? "Anonymous"
-                }
-            })
-        });
-
-
-
+        return res.status(200).render("/posts", { posts: allPosts });
 
     } catch (err) {
-        next(err);
+        return next(err);
 
     }
 });
 
+router.get("/:id", async (req: Request<{ id: string }>, res: Response, next: NextFunction) => {
+    const id: number = Number(req.params.id);
 
-router.use(async (err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (isNaN(id)) {
+        const err: HTTPError = new HTTPError("Post ID must be a number", 400);
+        err.name = "Bad Request";
+        return next(err);
+    }
+
+    if (id !== Math.floor(id)) {
+        const err: HTTPError = new HTTPError("Post ID must be an Integer", 400);
+        err.name = "Bad Request";
+        return next(err);
+    }
+
+    try {
+        const isMember: boolean = req.user?.member ?? false;
+
+        const post = await prisma.posts.findFirst({
+            where: {
+                id: id
+            },
+            include: {
+                user: isMember
+            }
+        });
+
+        return res.status(200).render("/posts", { posts: post })
+
+    } catch (err) {
+        return next(err);
+
+    }
+
+
+
+
+});
+
+
+
+
+
+
+router.use(async (err: HTTPError, req: Request, res: Response, next: NextFunction) => {
     console.error(err);
 
-    return res.send(`
-        Error Occurred: ${err.name}
+    return res.status(err.statusCode || 500).send(`
+        Error Occurred: ${err.name || "Internal server error"}
         ERROR: ${err.message}
         `);
 
